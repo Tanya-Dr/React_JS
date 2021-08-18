0a6c49da2b94dd96920701a95e92b81fac11f520
TataDr <druzhkova.tn@gmail.com>
2021-08-19 01:27:06 +0300

    delete saga_files

--- myapp_redux_saga/src/store/profile/actions.js
+++
@@ -1,6 +0,0 @@
-import { PROFILE_CHANGE_NAME } from "./actionTypes";
-
-export const changeName = (newName) => ({
-  type: PROFILE_CHANGE_NAME,
-  payload: newName,
-});
--- myapp_redux_saga/src/store/profile/selector.js
+++
@@ -1,4 +0,0 @@
-import { AUTHORS } from "../../constants";
-
-export const selectName = (state) =>
-  state.profile.name ? state.profile.name : AUTHORS.human;
